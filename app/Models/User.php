f4d2d9dc9a628ba5453af129cb52b08b137692f5
zyc <1978059479@qq.com>
2019-08-07 17:32:04 +0800

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,8 +38,14 @@ class User extends Authenticatable
 
 
     /**
-     *
+     * 使用全球通用头像，没有换国内cdn
      */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 
-    
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,8 @@ Route::get('/', function () {
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
 Route::get('/signup', 'UsersController@create')->name('signup');
-
-
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::resource('/users','UsersController');
 
 
